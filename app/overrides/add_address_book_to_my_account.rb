0da2eade95ba8f188e9a5fe68d0a876d55ac7f1f
Wooobee <m.laux@wooobee.com>
2014-11-04 22:25:30 +0100

    Changed selector and path

--- app/overrides/add_address_book_to_my_account.rb
+++ app/overrides/add_address_book_to_my_account.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
-  :virtual_path => "spree/users/show",
+  :virtual_path => "spree/shared/_account_header",
   :name => "address_book_account_my_orders",
-  :insert_after => "div#content",
+  :insert_after => "div.account-summary",
   :partial => "spree/users/addresses",
   :disabled => false
 )
