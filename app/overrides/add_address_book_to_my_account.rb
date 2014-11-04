0f26ec032251c3916af7877c45c41dfec845081d
Wooobee <m.laux@wooobee.com>
2014-11-04 22:07:17 +0100

    Update add_address_book_to_my_account.rb

--- app/overrides/add_address_book_to_my_account.rb
+++ app/overrides/add_address_book_to_my_account.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
   :virtual_path => "spree/users/show",
   :name => "address_book_account_my_orders",
-  :insert_after => ".account-summary",
+  :insert_after => "div#content",
   :partial => "spree/users/addresses",
   :disabled => false
 )
